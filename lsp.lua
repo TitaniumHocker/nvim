bd2fc0f37bb38df7ec32efb3ab6994b68f7c076a
Ivan Fedorov <inbox@titaniumhocker.ru>
2021-05-17 08:48:13 +0300

    Configured language servers

--- lsp.lua
+++ lsp.lua
@@ -1,3 +1,15 @@
-require('lspconfig').vimls.setup{on_attach=require('completion').on_attach}
-require('lspconfig').pyls.setup{on_attach=require('completion').on_attach}
-require('lspconfig').clangd.setup{on_attach=require('completion').on_attach}
+local lspconfig = require('lspconfig')
+
+lspconfig.vimls.setup{on_attach=require('completion').on_attach}
+lspconfig.clangd.setup{on_attach=require('completion').on_attach}
+lspconfig.perlls.setup{on_attach=require('completion').on_attach}
+lspconfig.html.setup{on_attach=require('completion').on_attach}
+lspconfig.tsserver.setup{on_attach=require('completion').on_attach}
+lspconfig.vuels.setup{on_attach=require('completion').on_attach}
+lspconfig.pyls.setup{
+    on_attach=require('completion').on_attach,
+    root_dir=lspconfig.util.root_pattern('Pipfile') 
+        or lspconfig.util.root_pattern('pyproject.toml')
+        or lspconfig.util.root_pattern('setup.py')
+        or lspconfig.util.root_pattern('.git'),
+}
